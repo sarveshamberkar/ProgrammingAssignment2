2dd279e6b700c8c5a1561e2d4c1c994ef6756fd7
sarveshamberkar <43892722+sarveshamberkar@users.noreply.github.com>
2020-06-13 20:00:43 +0530

    sha-1 encryption mentioned in description
    SHA-1=cfe827304466c991bf4e054807ef184c724b6c9e

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,16 @@
 makeCacheMatrix <- function(x = matrix()) {
   matrixinverse<-NULL
+  
   set<-function(y){
     x<<-y
     matrixinverse <<-NULL
+    ## change the value of inverse of the matrix in case the matrix was changed.
   }
+  
   get<-function()x
+  #calculates the inverse of non-singular matrix via the solve function
   setinverse<-function(solve){ matrixinverse <<-solve}
+   ## gets the value of the inverse
   getinverse<-function()matrixinverse
   list(set=set,get=get,
        setinverse=setinverse, getinverse=getinverse)
@@ -17,6 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   matrixinverse <-x$getinverse()
+  #if the inverse exists, it gets it.
   if(!is.null(matrixinverse)){
     message("getting cached data")
     return(matrixinverse)
